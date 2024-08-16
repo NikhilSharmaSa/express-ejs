ddbe6311b3d82113f89196f2297bb1acc7e3f365
Nikhil Sharma <nikhil.pandit.901@gmail.com>
2024-08-16 12:44:19 +0530

    upload files

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require('express')
+
+const app=express()
+const path=require('path')
+
+
+const port=3001
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'/views'))
+
+
+
+app.listen(port,()=>{
+
+    
+    console.log(`Port is Listening on ${port} `)})
+
+
+
+app.get('/',(req,res)=>{
+
+    const num=Math.floor(Math.random()*(9999-1000))+1000
+res.render('index',{num})
+})
